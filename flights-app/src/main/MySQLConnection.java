2660aac483cc8d919815e41905fbb09512f207f0
evelynngo14 <evelynngo14@gmail.com>
2025-11-05 17:16:36 -0500

    add sqlconnection

---
+++ flights-app/src/main/MySQLConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    public static void main(String[] args) {
+        String url = "mysql -h cis3270db.mysql.database.azure.com -P 3306 -u cis3270db -p;
+        String user = "cis3270db";
+        String password = "Administrator!";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connection to MySQL established successfully!");
+            // Perform database operations here
+        } catch (SQLException e) {
+            System.err.println("Error connecting to MySQL: " + e.getMessage());
+        }
+    }
+}
