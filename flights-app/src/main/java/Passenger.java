aea8d57b4fdec6b938725acb2431a9b391ec6149
evelynngo14 <evelynngo14@gmail.com>
2025-11-04 21:26:27 -0500

    edit classes

--- flights-app/src/main/java/Passenger.java
+++ flights-app/src/main/java/Passenger.java
@@ -1,29 +1,5 @@
 public class Passenger {
-    private String name;
-    private int ticket;
-    private String seat;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getTicket() {
-        return ticket;
-    }
-
-    public void setTicket(int ticket) {
-        this.ticket = ticket;
-    }
-
-    public String getSeat() {
-        return seat;
-    }
-
-    public void setSeat(String seat) {
-        this.seat = seat;
-    }
-}
+    private String firstName;
+    private String lastName;
+    private String mi;
+    private int flightNum;
