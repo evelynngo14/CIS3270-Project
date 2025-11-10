9bca1cbbb9e0a9b3fee46b5ca9362a06be4d2786
evelynngo14 <evelynngo14@gmail.com>
2025-11-10 02:54:26 -0500

    set up scenebuilder

--- flights-app/src/main/resources/gui/MainApp.java
+++ flights-app/src/main/resources/gui/MainApp.java
@@ -1,17 +1,22 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Parent root = loader.load();
         primaryStage.setTitle("Airline Reservation System");
-        primaryStage.setScene(new Scene(new Label("Welcome to the Airline App!"), 400, 200));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
