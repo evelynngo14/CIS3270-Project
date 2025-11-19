ffad9f74c4f9d74c248a6fe711a7bc442965a7c8
evelynngo14 <evelynngo14@gmail.com>
2025-11-18 21:22:02 -0500

    edit validate login

--- flights-app/src/main/program/dao/UserDAO.java
+++ flights-app/src/main/program/dao/UserDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import model.User;
+
 import java.sql.*;
 
 public class UserDAO {
@@ -7,7 +9,8 @@ public class UserDAO {
     private static final String dbUser = "src/main/cis3270db";
     private static final String dbPass = "Administrator!";
 
-    public static boolean validateLogin(String inputUsername, String inputPassword) {
+    // Authenticate user
+    public static User login(String inputUsername, String inputPassword) {
         String query = "SELECT * FROM users WHERE username = ? AND password = ?"; // ? is filled with user's input
 
         try (Connection conn = DriverManager.getConnection(url, dbUser, dbPass)) {
@@ -19,11 +22,16 @@ public class UserDAO {
 
             // Run the SQL query defined in stmt, and store the results in a ResultSet object called rs (table of rows)
             ResultSet rs = stmt.executeQuery(); // rs is handle to result set; use to loop through rows
-            return rs.next(); // true if user exists
+            if (rs.next()) { // true if user exists
+                User user = new User;
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
 
         } catch (SQLException e) {
             System.out.println("Login error: " + e.getMessage());
-            return false;
         }
+        return null;
     }
 }
