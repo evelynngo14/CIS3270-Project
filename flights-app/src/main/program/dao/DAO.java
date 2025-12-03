b4ead49ef0f82ca49003e3edb44b3e1e8d121f59
evelynngo14 <evelynngo14@gmail.com>
2025-12-03 09:16:26 -0500

    delete FlightsView

--- flights-app/src/main/program/dao/DAO.java
+++ flights-app/src/main/program/dao/DAO.java
@@ -3,6 +3,6 @@ package dao;
 public interface DAO {
     String dbUser = "cis";
     String dbPass = "Administrator!";
-    String url = String.format("jdbc:sqlserver://flights-app.database.windows.net:1433;database=flights-db;user=cis@flights-app;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", dbPass);
+    String url = String.format("jdbc:sqlserver://flights-app.database.windows.net:1433;database=flights-db;user=cis;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", dbPass);
 
 }
