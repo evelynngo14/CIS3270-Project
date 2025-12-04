0d723a9211990f5a6a8bacdc1688df3bcf031cb7
evelynngo14 <evelynngo14@gmail.com>
2025-12-04 14:46:13 -0500

    add gridpane structure with 2 columns to AddFlightView

--- flights-app/src/main/program/view/AddFlightView.java
+++ flights-app/src/main/program/view/AddFlightView.java
@@ -3,11 +3,14 @@ package view;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 
 import static java.lang.String.valueOf;
+
 
 public class AddFlightView extends VBox {
     String[] times = {"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
@@ -43,37 +46,52 @@ public class AddFlightView extends VBox {
         this.setPrefHeight(800);
 
         this.setSpacing(10);
+        // --------------
 
+        // Grid pane
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+
+        ColumnConstraints col1 = new ColumnConstraints(150); // Label column left
+        col1.setHalignment(javafx.geometry.HPos.LEFT);
+        ColumnConstraints col2 = new ColumnConstraints(300); // Field column left
+        col2.setHalignment(javafx.geometry.HPos.LEFT);
+        ColumnConstraints col3 = new ColumnConstraints(200); // Label column right
+        col3.setHalignment(javafx.geometry.HPos.LEFT);
+        ColumnConstraints col4 = new ColumnConstraints(250); // Field column right
+        col4.setHalignment(javafx.geometry.HPos.LEFT);
+
+        gridPane.getColumnConstraints().addAll(col1, col2, col3, col4);
+
+        // --------------
         //component properties
-
         HBox departureCityHBox = new HBox();
         departureCityHBox.getChildren().add(departureCityLabel);
         departureCityHBox.getChildren().add(departureCityField);
-        departureCityHBox.setAlignment(Pos.CENTER);
+        departureCityHBox.setAlignment(Pos.CENTER_LEFT);
         departureCityHBox.setSpacing(10);
-
-        HBox arrivalCityHBox = new HBox();
-        arrivalCityHBox.getChildren().add(arrivalCityLabel);
-        arrivalCityHBox.getChildren().add(arrivalCityField);
-        arrivalCityHBox.setAlignment(Pos.CENTER);
-        arrivalCityHBox.setSpacing(10);
 
         departureCityField.setMaxWidth(100);
         arrivalCityField.setMaxWidth(100);
         capacityField.setMaxWidth(50);
 
+        // --------------
+        //add components to gridpane
+        //left col
+        gridPane.add(departureCityLabel, 0, 0);
+        gridPane.add(departureCityField, 1, 0);
+        gridPane.add(arrivalCityLabel, 0, 1);
+        gridPane.add(arrivalCityField, 1, 1);
+
+        //right col
+        gridPane.add(capacityLabel, 2, 0);
+        gridPane.add(capacityField, 3, 0);
+
+        // --------------
 
         //add components to vbox
         this.getChildren().addAll(
-        headerLabel,
-        departureCityHBox,
-        arrivalCityHBox,
-        departureDateLabel, departureDatePicker,
-        departureTimeLabel, departureTimeChoice,
-        arrivalDateLabel, arrivalDatePicker,
-        arrivalTimeLabel, arrivalTimeChoice,
-        capacityLabel, capacityField,
-        addFlightButton, cancelButton
+        gridPane
         );
 
     }
