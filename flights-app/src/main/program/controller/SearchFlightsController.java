5511648d4ab3cce13d7cafc6393328a2559d0fe6
evelynngo14 <evelynngo14@gmail.com>
2025-12-06 13:05:04 -0500

    add fxml components in SearchFlightsController

--- flights-app/src/main/program/controller/SearchFlightsController.java
+++ flights-app/src/main/program/controller/SearchFlightsController.java
@@ -1,12 +1,21 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 
 public class SearchFlightsController {
 
     @FXML
     private ChoiceBox<String> arrivalCityList;
     @FXML private ChoiceBox<String> departureCityList;
+    @FXML private Button logoutButton;
+    @FXML private Button bookFlightsButton;
+    @FXML private DatePicker depDateChoiceBox;
+    @FXML private DatePicker arrDateChoiceBox;
+    @FXML private ChoiceBox<String> depTimeChoiceBox;
+    @FXML private ChoiceBox<String> arrTimeChoiceBox;
 
 }
